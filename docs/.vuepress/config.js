2ce9810ecb6369369a37ff7c9c8d81d8b9e674f3
liuji <1546243245@qq.com>
2023-08-21 02:50:54 +0800

    禁用sakura, 音乐盒

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -57,17 +57,17 @@ module.exports = {
         // ]
     },
     plugins: [
-        [
-            "sakura",
-            {
-                num: 20, // 默认数量
-                show: true, //  是否显示
-                zIndex: -1, // 层级
-                img: {
-                    replace: false, // false 默认图 true 换图 需要填写httpUrl地址
-                },
-            },
-        ],
+        // [
+        //     "sakura",
+        //     {
+        //         num: 20, // 默认数量
+        //         show: true, //  是否显示
+        //         zIndex: -1, // 层级
+        //         img: {
+        //             replace: false, // false 默认图 true 换图 需要填写httpUrl地址
+        //         },
+        //     },
+        // ],
         [
             "cursor-effects",
             {
@@ -76,31 +76,31 @@ module.exports = {
                 zIndex: 999999999, // z-index property of the canvas, default: 999999999
             },
         ],
-        [
-            "@vuepress-reco/vuepress-plugin-bgm-player",
-            {
-                audios: [
-                    {
-                        name: "我中意",
-                        artist: "魔动闪霸 / 炎明熹",
-                        url: "/我中意.mp3",
-                        // cover: "https://assets.smallsunnyfox.com/music/3.jpg",
-                    },
-                    // {
-                    //     name: "팔베개",
-                    //     artist: "최낙타",
-                    //     url: "https://assets.smallsunnyfox.com/music/3.mp3",
-                    //     cover:
-                    //         "https://p1.music.126.net/qTSIZ27qiFvRoKj-P30BiA==/109951165895951287.jpg?param=200y200",
-                    // },
-                ],
-                // 是否默认缩小
-                autoShrink: true,
-                // 缩小时缩为哪种模式
-                shrinkMode: "float",
-                // 悬浮窗样式
-                floatStyle: { bottom: "20px", "z-index": "999999" },
-            },
-        ],
+        // [
+        //     "@vuepress-reco/vuepress-plugin-bgm-player",
+        //     {
+        //         audios: [
+        //             {
+        //                 name: "我中意",
+        //                 artist: "魔动闪霸 / 炎明熹",
+        //                 url: "/我中意.mp3",
+        //                 // cover: "https://assets.smallsunnyfox.com/music/3.jpg",
+        //             },
+        //             // {
+        //             //     name: "팔베개",
+        //             //     artist: "최낙타",
+        //             //     url: "https://assets.smallsunnyfox.com/music/3.mp3",
+        //             //     cover:
+        //             //         "https://p1.music.126.net/qTSIZ27qiFvRoKj-P30BiA==/109951165895951287.jpg?param=200y200",
+        //             // },
+        //         ],
+        //         // 是否默认缩小
+        //         autoShrink: true,
+        //         // 缩小时缩为哪种模式
+        //         shrinkMode: "float",
+        //         // 悬浮窗样式
+        //         floatStyle: { bottom: "20px", "z-index": "999999" },
+        //     },
+        // ],
     ],
 }
