ac3960e21c67ee729e4b1273f55117f1cb71e6e6
liuji <1546243245@qq.com>
2023-08-21 03:12:03 +0800

    deploy

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -15,6 +15,7 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 
-git push -f git@github.com:lllllliuji/blogs.git master:gh-pages
+# git push -f git@github.com:lllllliuji/blogs.git master:gh-pages
+git push
 
 cd -
